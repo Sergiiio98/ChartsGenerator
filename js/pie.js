bc1e52446b51566fff4c669817a3815514efb893
Sergio S <alan.kazek@gmail.com>
2020-12-04 20:59:46 +0100

    Inputs done (values input, label input, chart name)

--- js/pie.js
+++ js/pie.js
@@ -3,26 +3,37 @@ const generateChartBtn = document.querySelector("#generateChart");
 
 
 generateChartBtn.onclick = function(){
-    let chartName = document.getElementById("h2cname").innerText;
+    let inputChartName = document.getElementById("cname").value;
+    document.getElementById("h2cname").innerText = inputChartName;
     
+    let values = [];
+    values[0] = document.getElementById("lname1").value;
+    values[1] = document.getElementById("lname2").value;
+    values[2] = document.getElementById("lname3").value;
+
+    let labels = [];
+    labels[0] = document.getElementById("arr0").value;
+    labels[1] = document.getElementById("arr1").value;
+    labels[2] = document.getElementById("arr2").value;
+    
+
     var ctx = document.getElementById("myChart").getContext('2d');
     var myChart = new Chart(ctx, {
   type: 'pie',
   data: {
-    labels: ["Green", "Blue", "Gray"],
+    labels: labels,
     datasets: [{
       backgroundColor: [
         "#2ecc71",
         "#3498db",
         "#95a5a6",
       ],
-      data: [25,35,40]
+      data: values
     }]
   }
 });
 
+
+
 }
-
-
-
 
