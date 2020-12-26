230a7938a4e984724789b6dc2bc745f3d455c43b
Sergio S <alan.kazek@gmail.com>
2020-12-26 01:53:23 +0100

    poprawki

--- js/bar.js
+++ js/bar.js
@@ -1,26 +1,43 @@
-var ctx = document.getElementById("myChart").getContext('2d');
-var barChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Monday'],
-			datasets: [{
-				label: 'Written Works',
-				backgroundColor: "red",
-				borderColor: "red",
-				data: [15,10,21,14,12],
-				fill: false,
-			}, {
-				label: 'Performance Tasks',
-				fill: false,
-				backgroundColor: "blue",
-				borderColor: "blue",
-				data: [17,15,27,14,15],
-			}, {
-				label: 'Quarterly Assessment',
-				fill: false,
-				backgroundColor: "green",
-				borderColor: "green",
-				data: [8,10,6,11,9],
-			}],
-  }
-});
+const generateChartBtn = document.querySelector("#generateBarChart");
+
+generateChartBtn.onclick = function(){
+
+	let inputChartName2 = document.getElementById("cname").value;
+    document.getElementById("cname3").innerText = inputChartName2;
+
+	let values = [];
+    values[0] = document.getElementById("lbname1").value;
+    values[1] = document.getElementById("lbname2").value;
+    values[2] = document.getElementById("lbname3").value;
+
+    let labels = [];
+    labels[0] = document.getElementById("barr0").value;
+    labels[1] = document.getElementById("barr1").value;
+	labels[2] = document.getElementById("barr2").value;
+	
+	
+	var ctx = document.getElementById("myChart").getContext('2d');
+	var barChart = new Chart(ctx, {
+		type: 'bar',
+		data: {
+		  labels: labels,
+		  datasets: [
+			{
+			  label: "",
+			  backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f"],
+			  data: values
+			}
+		  ]
+		},
+		options: {
+		  legend: { display: false },
+		  title: {
+			display: true,
+			text: ''
+		  }
+		},
+		
+	});
+}
+
+
--- js/pie.js
+++ js/pie.js
@@ -19,21 +19,18 @@ generateChartBtn.onclick = function(){
 
     var ctx = document.getElementById("myChart").getContext('2d');
     var myChart = new Chart(ctx, {
-  type: 'pie',
-  data: {
-    labels: labels,
-    datasets: [{
-      backgroundColor: [
-        "#2ecc71",
-        "#3498db",
-        "#95a5a6",
-      ],
-      data: values
-    }]
-  }
-});
-
-
-
+      type: 'pie',
+      data: {
+        labels: labels,
+        datasets: [{
+          backgroundColor: [
+            "#2ecc71",
+            "#3498db",
+            "#95a5a6",
+          ],
+          data: values
+        }]
+      }
+    });
 }
 
